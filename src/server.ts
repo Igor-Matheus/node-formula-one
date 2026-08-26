4f9499f0e524b1562d5d2136e0800f7c6a907337
Igor <igo.mat@hotmail.com>
2026-08-26 17:32:05 -0300

    update server.ts

--- src/server.ts
+++ src/server.ts
@@ -25,29 +25,41 @@ server.get<{ Params: DriverParams }>(
   "/drivers/:id",
   async (request, response) => {
     const id = parseInt(request.params.id);
+
+    if (isNaN(id)) {
+      return response.code(400).send({ message: "Invalid ID format" });
+    }
+
     const driver = drivers.find((d) => d.id === id);
 
     if (!driver) {
-      response.type("application/json").code(404);
-      return { message: "Driver not found" };
-    } else {
-      response.type("application/json").code(200);
-      return { driver };
+      return response
+        .type("application/json")
+        .code(404)
+        .send({ message: "Driver not found" });
     }
+
+    return response.type("application/json").code(200).send({ driver });
   },
 );
 
 server.get<{ Params: TeamParams }>("/teams/:id", async (request, response) => {
   const id = parseInt(request.params.id);
+
+  if (isNaN(id)) {
+    return response.code(400).send({ message: "Invalid ID format" });
+  }
+
   const team = teams.find((t) => t.id === id);
 
   if (!team) {
-    response.type("application/json").code(404);
-    return { message: "Team not found" };
-  } else {
-    response.type("application/json").code(200);
-    return { team };
+    return response
+      .type("application/json")
+      .code(404)
+      .send({ message: "Team not found" });
   }
+
+  return response.type("application/json").code(200).send({ team });
 });
 
 server.listen({ port: 3333 }, () => {
